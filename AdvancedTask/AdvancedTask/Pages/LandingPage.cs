a041bf4a5dede9fd897c1efd99207b2366523191
Shilpii01 <shilpicharu@gmail.com>
2024-02-02 10:37:26 +1300

    first commit

---
+++ AdvancedTask/AdvancedTask/Pages/LandingPage.cs
@@ -0,0 +1,45 @@
+﻿using AdvancedTask.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTask.Pages
+{
+    public class LandingPage: BaseClass
+    {
+        private IWebElement signInButton;
+
+
+        public void renderComponents()
+        {
+            try
+            {
+                signInButton = driver.FindElement(By.XPath("//*[text()='Sign In']"));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+
+        public void clickSignInButton()
+        {
+
+            //Click on "Sign In" button
+            Wait.WaitToBeClickable(driver, "XPath", "//*[text()='Sign In']", 10);
+
+            renderComponents();
+            signInButton.Click();
+
+        }
+
+
+
+    }
+}
